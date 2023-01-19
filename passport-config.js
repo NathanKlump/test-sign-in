6e728e5c8a576489c6a4fcae29533d03ab682c64
NathanKlump <98673875+NathanKlump@users.noreply.github.com>
2023-01-19 12:53:14 -0500

    initial commit

---
+++ passport-config.js
@@ -0,0 +1,11 @@
+const { authenticate } = require('passport')
+
+const LocalStrategy = require('passport-local').Strategy
+
+function initialize(passport) {
+    const authenticateUser = () => {
+        
+    }
+    passport.use(new LocalStrategy({ usernameField: 'email' }), 
+    authenticateUser)
+}
