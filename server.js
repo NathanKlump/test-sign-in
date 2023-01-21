137d8b85e3e2b851d3dbb759c77bbf95d4f0ad9b
NathanKlump <98673875+NathanKlump@users.noreply.github.com>
2023-01-21 16:36:45 -0500

    added node modules, could be a mistake

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-  
   const express = require('express')
   const app = express()
   const bcrypt = require('bcrypt')
@@ -21,11 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.set('view-engine', 'ejs')
   app.use(express.urlencoded({ extended: false }))
   app.use(flash())
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-  }))
+  app.use(session({ secret: 'mysecretkey', resave: false, saveUninitialized: false }))
   app.use(passport.initialize())
   app.use(passport.session())
   
